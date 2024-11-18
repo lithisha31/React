87aa66980a924e1ace8b1543765f284a1ae4a5d2
lithisha31 <lithishamandappa31@gmail.com>
2024-11-18 20:41:21 +0530

    Revert "Delete project-forms directory"
    This reverts commit dc12858f737d0145c7118a28582a95e25dfd2fef.

---
+++ project-forms/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Forms from './Forms';
+import BetterForm from './BetterForm';
+import ShoppingListForm from './ShoppingListForm';
+import ShoppingList from './ShoppingList';
+
+function App() {
+  return (
+    <div className="App">
+      <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
