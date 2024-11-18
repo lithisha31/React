e547767913b02ed3a7a201485bbd60218d810138
lithisha31 <lithishamandappa31@gmail.com>
2024-11-18 23:46:07 +0530

    Changes made to LuckyN and BoxGrid component

--- project-03/src/LuckyN.jsx
+++ project-03/src/LuckyN.jsx
@@ -6,10 +6,14 @@ import Dice from "./Dice";
 export default function LuckyN( {numDice = 2, goal=7}){
     const [dice, setDice] = useState(getRolls(numDice));
     console.log(dice);
+    const reroll = () =>{
+        setDice(getRolls(numDice))
+    }
     return(
         <main className = "LuckyN">
             <h1> LuckyN </h1>
             <Dice dice={dice} />
+            <button onClick={reroll}>Re-Roll</button>
         </main>
     );
 }
